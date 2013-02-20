0e94c545017460fd33903a09f5707780f531836e
Bilal Quadri <bilalquadri92@gmail.com>
2013-02-20 14:40:57 -0500

    Filter stubbed out for restricting admin access

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,15 @@
 class AdminController < ApplicationController
 
+  before_filter :require_admin
+
+  def require_admin
+    # TODO: this is dependent on users existing
+    #unless @user and @user.is_admin?
+      #flash[:error] = "Only admins can access this page"
+      #return redirect_to '/login'
+    #end
+  end
+
   def index
     respond_to do |format|
       format.html # index.html.erb
