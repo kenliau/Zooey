90a6638d99ce5e56c62fca313effd9c192995295
Bilal Quadri <bilalquadri92@gmail.com>
2013-02-28 12:44:37 -0500

    Filtering and routes for admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,17 +3,25 @@ class AdminController < ApplicationController
   before_filter :require_admin
 
   def require_admin
-    # TODO: this is dependent on users existing
-    #unless @user and @user.is_admin?
-      #flash[:error] = "Only admins can access this page"
-      #return redirect_to '/login'
-    #end
+    unless user_signed_in? and current_user.is_admin?
+      flash[:error] = "Only admins can access this page"
+      return redirect_to '/login'
+    end
   end
 
   def index
     respond_to do |format|
       format.html # index.html.erb
     end
+  end
+
+  def users
+  end
+
+  def videos
+  end
+
+  def jobs
   end
 
 end
