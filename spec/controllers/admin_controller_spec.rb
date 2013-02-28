9c9fd02ea34ef234411a37f7a1474ff4e69e7b83
Bilal Quadri <bilalquadri92@gmail.com>
2013-02-28 14:28:29 -0500

    Started writing test for admin controller

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -2,4 +2,28 @@ require 'spec_helper'
 
 describe AdminController do
 
+  describe 'Admin only filter' do
+
+    it 'should redirect guests to home' do
+      get 'index'
+      response.should be_redirect
+    end
+
+    it 'should redirect non-admins to home' do
+      @user = Factory.build(:user)
+      sign_in @user
+      get 'index'
+      response.should be_redirect
+    end
+
+    it 'should allow admins to access actions in AdminController' do
+      @user = Factory.build(:user)
+      @user.is_admin = true
+      sign_in @user
+      get 'index'
+      response.should be_success
+    end
+
+  end
+
 end
