bb76a6f5db9454a375338862dd39233a20b873f1
Bilal Quadri <bilalquadri92@gmail.com>
2013-03-11 19:26:49 -0400

    Wrote validations for jobs and videos

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,15 @@
 class Job < ActiveRecord::Base
   belongs_to :video
   attr_accessible :audio_bitrate, :audio_codec, :audio_volume, :finished_at, :h264_profile, :h264_quality, :height, :mux_rate, :width
+
+  validates :audio_bitrate, :audio_codec, :audio_volume, :h264_profile, :h264_quality, :height, :mux_rate, :width,
+    :presence => {
+      message: "is a required field"
+    }
+
+  validates :height, :width, :audio_codec, :audio_bitrate, :audio_volume, :mux_rate,
+    :numericality => {
+      :only_integer => true,
+      :greater_than_or_equal_to => 0
+    }
 end
