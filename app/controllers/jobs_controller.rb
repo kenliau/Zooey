8b4358ed40691dad9a0ac2c3da37095a8812b78f
Bilal Quadri <bilalquadri92@gmail.com>
2013-03-28 06:13:24 -0400

    Wrote filters for jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,8 +1,21 @@
 class JobsController < ApplicationController
-  before_filter :authenticate_user!, :owns_job_or_admin
+  before_filter :authenticate_user!
+  before_filter :load_job, :only => [:show, :destroy]
+  before_filter :owns_job_or_admin, :only => [:show, :destroy]
+
+  # Sets instance variable for job and checks if it's blank
+  def load_job
+    @job = Job.includes(:progression).find(params[:id])
+    if @job.blank?
+      return redirect_to '/jobs'
+    end
+  end
 
   # Requires current user to own job or be admin
   def owns_job_or_admin 
+    unless @job.video.user == current_user or current_user.is_admin
+      return redirect_to '/jobs'
+    end
   end
 
   # GET /jobs
@@ -18,8 +31,6 @@ class JobsController < ApplicationController
   # GET /jobs/1
   # GET /jobs/1.json
   def show
-    @job = Job.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @job }
@@ -29,9 +40,7 @@ class JobsController < ApplicationController
   # DELETE /jobs/1
   # DELETE /jobs/1.json
   def destroy
-    @job = Job.find(params[:id])
     @job.destroy
-
     respond_to do |format|
       format.html { redirect_to jobs_url }
       format.json { head :no_content }
--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
 describe JobsController do
+  include Devise::TestHelpers
+
+  describe 'Authentication filter' do
+
+    context 'when user is guest' do
+      it 'should redirect when trying to reach index' do
+        get 'index'
+        response.should be_redirect
+      end
+
+      it 'should redirect when trying to reach show' do
+        get 'show', id: 1
+        response.should be_redirect
+      end
+
+      it 'should redirect when trying to reach destroy' do
+        delete 'destroy', id: 1
+        response.should be_redirect
+      end
+    end
+
+    #context 'when user is signed in and not an admin' do
+      #user = FactoryGirl.create(:user, :email => 'nonadminjobs@email.com')
+      #sign_in user
+
+      #it 'should allow index to load index' do
+        #get 'index'
+        #response.should_not be_success
+      #end
+    #end
+
+  end
+
+  describe 'Load Job filter' do
+
+    context 'should redirect if no job was found' do
+
+    end
+  end
 
 end
