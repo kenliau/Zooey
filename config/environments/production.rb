85d0edef6db3d023ffdcd67bcdb9ab58946f7784
Devon Peticolas <devon.peticolas@gmail.com>
2013-03-28 12:09:31 -0400

    fixed production redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ Zipreel::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  # Redis To Go URL for Heroku
+  ENV["REDISTOGO_URL"] = 'redis://redistogo:67de122c22b0c035a871c5d5853c2062@spinyfin.redistogo.com:9023'
+
 end
