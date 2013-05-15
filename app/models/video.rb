e997465818a10b60b40cf005460a92228ca57562
Kenneth Liau <liauken@gmail.com>
2013-05-15 17:07:43 -0400

    Added new attr in video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,7 +3,7 @@ class Video < ActiveRecord::Base
   default_scope order('created_at DESC')
   belongs_to :user
   has_many :jobs, :dependent => :destroy
-  attr_accessible :duration, :video_name, :filename, :location, :size
+  attr_accessible :duration, :video_name, :filename, :location, :size, :video_codec, :width, :height, :video_bitrate, :frames_per_sec
 
   validates :location,
     :presence => {
