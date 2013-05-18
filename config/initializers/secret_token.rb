29c93fd22ae6edc7514a411a01a408b489c219d9
Kenneth Liau <liauken@gmail.com>
2013-05-18 13:06:45 -0400

    removed token'

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Zipreel::Application.config.secret_token = 'b7728793451c98db4d79482e9036fdc008cd31f5d39e95c53a3688b912cc8f636763ff0767fb122b9ebc0816436514f7b55dfdee67f4c313772b3bde7c7dae42'
