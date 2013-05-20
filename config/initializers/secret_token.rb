1d4b8becaa05c4f5ac95e5bef12c0b7162623472
Kenneth Liau <liauken@gmail.com>
2013-05-20 02:24:51 -0400

    Better secret_token handling

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Zooey::Application.config.secret_token = ENV['ZOOEY_SECRET_TOKEN'] || 'bbed8355967d9e542f84a19b055774a990a701d05f516670823974378d2aefc58a1547b4e7709979c923ffb94a3d2f7e8a08238fbaecf790858e7ecf376ee4e1'
+
+Zooey::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  'bbed8355967d9e542f84a19b055774a990a701d05f516670823974378d2aefc58a1547b4e7709979c923ffb94a3d2f7e8a08238fbaecf790858e7ecf376ee4e1'
+else
+  ENV['ZOOEY_SECRET_TOKEN']
+end
